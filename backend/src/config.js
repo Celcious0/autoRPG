213fd656e7128eeb7ee1011e0c33512700485e07
Celcious <celcious1@naver.com>
2025-07-20 11:24:39 +0900

    0.2.6
    0.2.6

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,17 +1,29 @@
 import 'dotenv/config';
 
+const firebaseServiceJson = process.env.FIREBASE_SERVICE_ACCOUNT_JSON
+  ? JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON)
+  : {};
+
 export default {
   port: process.env.PORT || 3000,
+
   firebase: {
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\n/g, '\n')
+    projectId: firebaseServiceJson.project_id,
+    clientEmail: firebaseServiceJson.client_email,
+    privateKey: firebaseServiceJson.private_key?.replace(/\\n/g, '\n')
   },
+
   mysql: {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASS,
+    password: process.env.DB_PASSWORD, // 수정됨
     database: process.env.DB_NAME,
+    port: +process.env.DB_PORT || 3306,
+    ssl: process.env.DB_SSL_CA
+      ? {
+          ca: process.env.DB_SSL_CA
+        }
+      : undefined,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
